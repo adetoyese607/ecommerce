e4e93e76d689a2c281416cf0d9c6864fb9a753e5
Matthew <adetoyesesamson@gmail.com>
2026-01-30 13:14:34 +0100

    Homepage Screen done

--- lib/screens/homepage_screen.dart
+++ lib/screens/homepage_screen.dart
@@ -1,6 +1,6 @@
 // ignore_for_file: deprecated_member_use
 
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomepageScreen extends StatefulWidget {
@@ -16,6 +16,7 @@ class _HomepageScreenState extends State<HomepageScreen> {
     'assets/image1.jpg',
     'assets/image2.jpg',
     'assets/image3.jpg',
+    'assets/image2.jpg',
   ];
   List<String> productTitles = [
     'Warm Zipper',
@@ -132,7 +133,7 @@ class _HomepageScreenState extends State<HomepageScreen> {
                 ),
                 SizedBox(height: 20),
                 Container(
-                  height: 300,
+                  height: 180,
                   // color: Colors.red,
                   child: ListView.builder(
                     shrinkWrap: true,
@@ -141,17 +142,17 @@ class _HomepageScreenState extends State<HomepageScreen> {
                     itemBuilder: (context, index) {
                       return Container(
                         margin: EdgeInsets.only(right: 20),
-                        child: Column(
-                          // mainAxisAlignment: MainAxisAlignment.start,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
-                            Container(
-                              height: 200,
+                            SizedBox(
+                              // height: 180,
                               // width: 200,
                               child: Stack(
                                 children: [
                                   AspectRatio(
-                                    aspectRatio: 1 / 1.5,
+                                    aspectRatio: 1.4 / 1.5,
                                     child: Container(
                                       decoration: BoxDecoration(
                                         borderRadius: BorderRadius.circular(10),
@@ -184,21 +185,140 @@ class _HomepageScreenState extends State<HomepageScreen> {
                                 ],
                               ),
                             ),
-                            SizedBox(height: 10,),
+                            SizedBox(width: 10),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  productTitles[index],
+                                  style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black,
+                                    fontSize: 16,
+                                  ),
+                                ),
+                                SizedBox(height: 10),
+                                SizedBox(
+                                  width: 120,
+                                  child: Text(
+                                    'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600
+                                    ),
+                                    maxLines: 7,
+                                    overflow: TextOverflow.ellipsis,
+                                  ),
+                                ),
+
+                                SizedBox(height: 10),
+                                Row(
+                                  children: [
+                                    Icon(
+                                      Icons.star,
+                                      color: Colors.yellow,
+                                      size: 18,
+                                    ),
+                                    Text('(${reviews[index]})'),
+                                    SizedBox(width: 5),
+                                    Text(
+                                      prices[index],
+                                      style: TextStyle(
+                                        color: Color.fromARGB(255, 182, 13, 13),
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                 SizedBox(height: 30),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text('Newest Products', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 26   ),)),
+                SizedBox(height: 20),
+                Container(
+                  child: GridView.builder(
+                    shrinkWrap: true,
+                    // scrollDirection: Axis.horizontal,
+                    itemCount: productTitles.length,
+                    physics: NeverScrollableScrollPhysics(),
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: 2,
+                      childAspectRatio: 0.7,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Container(
+                        // height: 200,
+                        // margin: EdgeInsets.only(right: 20),
+                        child: Column(
+                          // mainAxisAlignment: MainAxisAlignment.start,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            SizedBox(
+                              height: 180,
+                              // width: 250,
+                              child: Stack(
+                                children: [
+                                  AspectRatio(
+                                    aspectRatio: 1.05 / 1,
+                                    child: Container(
+                                      decoration: BoxDecoration(
+                                        borderRadius: BorderRadius.circular(10),
+                                        image: DecorationImage(
+                                          image: AssetImage(
+                                            productImages[index],
+                                          ),
+                                          fit: BoxFit.cover,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                  Positioned(
+                                    top: 10,
+                                    right: 10,
+
+                                    child: Container(
+                                      height: 30,
+                                      width: 30,
+                                      decoration: BoxDecoration(
+                                        shape: BoxShape.circle,
+                                        color: Colors.white,
+                                      ),
+                                      child: Icon(
+                                        Icons.favorite_outlined,
+                                        color: Color.fromARGB(255, 182, 13, 13),
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            SizedBox(height: 10),
                             Text(
                               productTitles[index],
                               style: TextStyle(
                                 fontWeight: FontWeight.bold,
                                 color: Colors.black,
-                                fontSize: 16
+                                fontSize: 16,
                               ),
                             ),
                             SizedBox(height: 10),
                             Row(
                               children: [
-                                Icon(Icons.star, color: Colors.yellow, size: 18,),
+                                Icon(
+                                  Icons.star,
+                                  color: Colors.yellow,
+                                  size: 18,
+                                ),
                                 Text('(${reviews[index]})'),
-                                SizedBox(width: 5,),
+                                SizedBox(width: 5),
                                 Text(
                                   prices[index],
                                   style: TextStyle(
